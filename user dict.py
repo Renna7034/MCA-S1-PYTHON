4ffcd78bcee809085ea432924d084007696936c6
Renna7034 <150773043+Renna7034@users.noreply.github.com>
2024-01-23 22:37:33 +0530

    Add files via upload

---
+++ user dict.py
@@ -0,0 +1,8 @@
+user_dict = {}
+n=int(input("enter the limit of dict"))
+for i in range(n):
+    key = input("Enter key: ")
+    value = input("Enter value: ")
+    user_dict[key] = value
+
+print(user_dict)
